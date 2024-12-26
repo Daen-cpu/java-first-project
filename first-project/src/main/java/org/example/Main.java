c09ac8918fc88aaa35186493ed1008b6472ea97f
Diana@HP-0005 <yadekina.diana@mail.ru>
2024-12-26 11:36:40 +0300

    Реализован поиск максимума их четырех чисел

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,18 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
+        int a = 5, b = 3, c = 9, d = 7;
+        int max = a;
 
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
 
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
-
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        System.out.println("Максимум: " + max);
     }
 }
